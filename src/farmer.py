df34a47adf4dd8c14602439bbfad3e847a8e601a
Ajaydeep singh <ajaydeep@Ajaydeeps-MacBook-Pro.local>
2018-07-13 02:50:05 -0700

    bs4 for scraping & inital commit

---
+++ src/farmer.py
@@ -0,0 +1,17 @@
+import bs4 as bs
+import urllib.request
+
+SERVER = ""
+USER_NAME = ""
+POLLING_TIME = 0
+
+def runner():
+	print("I love you and you love me");
+
+
+
+
+
+
+if __name__ == '__main__':
+	runner()
